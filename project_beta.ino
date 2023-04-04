d1ce622a4516fcca52eeb688c9d0b21470988502
cmespineli <126132263+cmespineli@users.noreply.github.com>
2023-04-04 01:22:20 -0500

    Going off Project 1 code
    I uploaded project 1 code to be our base since there is some code we can use. Just took out the lines that we for sure aren't using.

--- project_beta.ino
+++ project_beta.ino
@@ -2,39 +2,22 @@
 
 const int pushButton = 2;
 const int led = 6;
-const int sensor = A0;
-const int piezo = 8;
+
 int previousButtonState;
 int presentButtonState;
 int ledState = LOW;
-int sensorVal;
 
 void setup() {
   pinMode(pushButton, INPUT);
-  pinMode(sensor, INPUT);
   pinMode(led, OUTPUT);
-  pinMode(piezo, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   previousButtonState = presentButtonState;
   presentButtonState = digitalRead(pushButton);
-  int sensorStatus = analogRead(sensor);
-  Serial.println(sensorStatus);
 
   if (previousButtonState == HIGH && presentButtonState == LOW) {
     ledState = !ledState;
     digitalWrite(led, ledState);
   }
-
-  if (ledState = ledState) {
-    sensorVal = analogRead(sensor);
-    Serial.println(sensorVal);
-    if (sensorVal <= 1000) {
-      digitalWrite(piezo, HIGH);
-    } else {
-      digitalWrite(piezo, LOW);
-    }
-  }
-}
